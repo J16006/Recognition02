ad7095dfa6d3fc92c170b30f32766a34e43d9d24
nykin <nykin@DESKTOP-FOJAC5C>
2018-06-07 21:53:18 +0900

    課題2

--- src/recognition02/Recognition02.java
+++ src/recognition02/Recognition02.java
@@ -19,6 +19,7 @@ public class Recognition02 {
 		VisualRecognition service = new VisualRecognition("2018-03-19");
 		service.setApiKey("49d9fa9ea3694ffdcb2ec3a9812f7976a5e6d904");
 
+		MySQL mysql = new MySQL();
 		InputStream imagesStream = null;
 		try {
 			imagesStream = new FileInputStream("./img/fruitbowl.jpg");
@@ -43,20 +44,22 @@ public class Recognition02 {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		String class_fruit=node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").asText();
-		Double class_score = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").asDouble();
-		String class_fruit1=node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").asText();
-		Double class_score1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").asDouble();
-		String class_fruit2=node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").asText();
-		Double class_score2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").asDouble();
+		String class1=node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").toString();
+		Double score1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").asDouble();
+		String class2=node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").toString();
+		Double score2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").asDouble();
+		String class3=node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").toString();
+		Double score3 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").asDouble();
 
 
-		System.out.println(class_fruit);
-		System.out.println(class_score);
-		System.out.println(class_fruit1);
-		System.out.println(class_score1);
-		System.out.println(class_fruit2);
-		System.out.println(class_score2);
+		System.out.println(class1);
+		System.out.println(score1);
+		System.out.println(class2);
+		System.out.println(score2);
+		System.out.println(class3);
+		System.out.println(score3);
+		mysql.updateImage(class1,score1,class2,score2,class3,score3);
+
 
 
 	}
